9ed02695a11555b99f6bd0c458ec384843cc3a2a
rafaelcode0337 <78993486+rafaelcode0337@users.noreply.github.com>
2021-12-11 18:05:44 -0300

    Update ListFiltering.py

--- List Filtering/ListFiltering.py
+++ List Filtering/ListFiltering.py
@@ -1,2 +1,5 @@
 def filter_list(l):
-  return list(filter(lambda item: not type(item) == str, l))
+    return list(filter(lambda item: not type(item) == str, l))
+
+def filter_list2(l):
+    return [item for item in l if not isinstance(item, str)]
